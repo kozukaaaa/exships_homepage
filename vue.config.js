399958e134471b0ae701b57088ba6485fbaaf301
kozukaaaa <aosugi0405@gmail.com>
2026-03-20 16:30:21 +0900

    feat: change-hash-history docs

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,15 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: "./",
-  assetsDir: "./",
+  publicPath: "/",
+  assetsDir: "assets",
   outputDir: "docs",
   transpileDependencies: true,
+
+  devServer: {
+    host: "0.0.0.0",
+    port: 8082,
+    allowedHosts: "all",
+  },
 
   pluginOptions: {
     vuetify: {
